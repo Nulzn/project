aa0e9f4a2c8682261b1943e802ba80c363a5a1fa
Nulzn <68256766+Nulzn@users.noreply.github.com>
2023-09-23 21:07:45 +0200

    First Commit

--- main.cpp
+++ main.cpp
@@ -1,29 +1,11 @@
 #include <iostream>
+#include <vector>
 #include <fstream>
 #include <algorithm>
-#include <ostream>
-#include <vector>
 
 using namespace std;
 int main() {
-	ifstream planetsFile;
-	planetsFile.open("words.txt");
-	string word;
-	vector<string> planetsArr = {};
 
-	while (planetsFile >> word)
-		planetsArr.push_back(word);
-
-	sort(begin(planetsArr), end(planetsArr));
-	planetsFile.close();
-
-	ofstream newPlanetFile;
-	newPlanetFile.open("words.txt", ofstream::out | ofstream::trunc);
-
-	for (string planet : planetsArr)
-		newPlanetFile << "Planet: " << planet << "\n";
-
-	newPlanetFile.close();
-
+	cout << "Hello, World!" << "\n";
 	return 0;
 }
